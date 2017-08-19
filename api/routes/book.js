7267831e405f130fd2eb4cc250a239eccdd4b55a
ChaosStream <ChaosStream@chaosenterprise.com>
2017-08-19 19:57:13 -0400

    Cleaning up Keikiis changes

--- api/routes/book.js
+++ api/routes/book.js
@@ -5,5 +5,8 @@ var controller = require('../controllers/book')
 
 router.get('/', controller.list());
 router.post('/', controller.create());
+router.delete("/:id", controller.del());
+router.get("/:id", controller.get());
+router.put("/:id", controller.put());
 
 module.exports = router;
--- public/javascripts/keikii.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-angular.module('keikii', ['ngRoute', 'ngAnimate', 'ngSanitize', 'ui.bootstrap',
-	'blockUI']);
